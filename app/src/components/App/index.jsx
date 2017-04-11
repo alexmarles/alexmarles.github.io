e7155ff77dc693ead82aeef31468e1c9c784bad6
Àlex Marlés <alex@alexmarles.com>
2017-04-11 17:22:07 +0200

    First components

---
+++ app/src/components/App/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import 'normalize-css'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>React Webpage</h1>
+      </div>
+    )
+  }
+}
+
+export default App
