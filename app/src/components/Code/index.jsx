31f5c2d2f9c1af9da5ac7d265875e09d923f1480
Àlex Marlés <alex@alexmarles.com>
2017-04-12 17:19:07 +0200

    Added return link

--- app/src/components/Code/index.jsx
+++ app/src/components/Code/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Code extends Component {
   render () {
     return (
       <div>
         <h1>Code</h1>
+        <Link to='/'>Return</Link>
       </div>
     )
   }
